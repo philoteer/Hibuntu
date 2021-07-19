1b0ca50d56771fab057f134fcbb43b78fb9b658e
philoteer <cdac1234@gmail.com>
2021-07-19 13:07:15 +0900

    Update sdcard-bootstrap.sh

--- sdcard-bootstrap.sh
+++ sdcard-bootstrap.sh
@@ -2,6 +2,7 @@
 #adapted from: https://archlinuxarm.org/platforms/armv7/rockchip/hisense-chromebook-c11 
 #set -x
 DEV="/dev/mmcblk0"
+ROOTFS= "/dev/mmcblk0p2"
 
 chk="`cgpt show -i 10 ${DEV} | grep Unused`"
 if [ "$chk" = "" ]; then
@@ -25,3 +26,10 @@ rootfs_pos="`cgpt show ${DEV} | grep \"Sec GPT table\" | awk '{print $1}'`"
 echo $rootfs_pos
 cgpt add -i 2 -t data -b 40960 -s `expr ${rootfs_pos} - 40960` -l Root ${DEV}
 partx -a ${DEV}
+
+mkfs.ext4 ${ROOTFS}
+mkdir /tmp/mnt
+mount ${ROOTFS} /tmp/mnt
+debootstrap --arch=armhf --foreign bionic /tmp/mnt http://ports.ubuntu.com/ubuntu-ports
+sync
+umount ${ROOTFS}
